fc9b8e5280628347196175c639fddc8612c19eef
tangtikon <tangtikon@gmail.com>
2022-03-11 10:45:00 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect('localhost','root','99109911lovE','db_journal');
+$conn = mysqli_connect('localhost','root','','db_journal');
 mysqli_set_charset($conn, "utf8");
 // if(!$conn){
 //     die("Cannot connect to the database. Error: ".mysqli_error($conn));
